0bebe81c2b69d50d7e564ca4bf0e9bf2caf07f3d
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-12-23 09:52:12 +0530

    first commit

--- src/app/[userId]/page.tsx
+++ src/app/[userId]/page.tsx
@@ -10,16 +10,12 @@ export default function Home() {
         <Hero/>
         <MaxWidthWrapper>
         <ProductReel
-<<<<<<< HEAD
-          query={{ limit: 4,search:'',price:1500,label:"both",order:-1 }}
-=======
           query={{ limit: 4,search:'',price:1500,label:'both'}}
->>>>>>> bcccbc013a73ceac52a53aa4896491fe3f7f79a8
           href='/products?sort=recent'
           title='Brand new'
         />
         </MaxWidthWrapper>
-        
+
     </div>
   )
 }
